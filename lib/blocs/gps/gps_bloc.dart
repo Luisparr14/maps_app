1821e44e38d5701705f24991f5b3956aa870cbf9
Luis Angel <luisparr14@gmail.com>
2023-04-15 17:12:28 -0500

    clase #188 - flutter avanzado Udemy

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -13,5 +14,25 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
           isGpsEnable: event.isGpsEnable,
           isGpsPermissionGranted: event.isGpsPermissionGranted));
     });
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    _checkLocationStatus();
+  }
+
+  Future<bool> _checkLocationStatus() async {
+    final isLocationEnable = await Geolocator.isLocationServiceEnabled();
+    Geolocator.getServiceStatusStream().listen((event) {
+      // TODO: Fire events
+    });
+    return isLocationEnable;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: Clean Service Status Listener
+    return super.close();
   }
 }
