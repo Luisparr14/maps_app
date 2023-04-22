f0f5b4e6f7c20a782ed1172ac2da80ec1bf35d19
Luis Angel <luisparr14@gmail.com>
2023-04-22 09:53:43 -0500

    clase #232 - Flutter Avanzado - Udemy

--- lib/widgets/manual_marker.dart
+++ lib/widgets/manual_marker.dart
@@ -13,13 +13,62 @@ class ManualMarker extends StatelessWidget {
       height: size.height,
       child: Stack(
         children: [
+          const Positioned(top: 40, left: 10, child: _BtnBack()),
           Center(
             child: Transform.translate(
-              offset: const Offset(0, -15),
-              child: BounceInDown(child: const Icon(Icons.location_on_rounded, size: 40))
-              ),
-          )
+                offset: const Offset(0, -15),
+                child: BounceInDown(
+                    child: const Icon(Icons.location_on_rounded, size: 40))),
+          ),
+          const Positioned(bottom: 40, left: 40, child: _BtnConfirmLocation()),
         ],
+      ),
+    );
+  }
+}
+
+class _BtnConfirmLocation extends StatelessWidget {
+  const _BtnConfirmLocation();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return FadeInUp(
+      duration: const Duration(milliseconds: 300),
+      child: MaterialButton(
+          onPressed: () {},
+          color: Colors.black87,
+          minWidth: size.width - 110,
+          height: 40,
+          elevation: 1,
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          shape: const StadiumBorder(),
+          child: const Text('Confirm Location',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15))),
+    );
+  }
+}
+
+class _BtnBack extends StatelessWidget {
+  const _BtnBack();
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInLeft(
+      duration: const Duration(milliseconds: 300),
+      child: CircleAvatar(
+        backgroundColor: Colors.black87,
+        minRadius: 18,
+        child: IconButton(
+            onPressed: () {
+              print('Back');
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_rounded)),
       ),
     );
   }
