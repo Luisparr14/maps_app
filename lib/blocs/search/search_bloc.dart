b5106a80bd04468ec6c5ed8d9b1c447c85c2f58d
Luis Angel <luisparr14@gmail.com>
2023-04-22 14:29:33 -0500

    clase #242 - Flutter Avanzado - Udemy

--- lib/blocs/search/search_bloc.dart
+++ lib/blocs/search/search_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 import 'package:maps_app/models/models.dart';
 import 'package:maps_app/services/services.dart';
 
@@ -16,6 +17,18 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
         emit(state.copyWith(displayManualMarker: event.activate)));
   }
 
-  Future<TrafficResponse> getCoordsStartToEnd(LatLng start, LatLng end) async =>
-      await trafficService.getCoordsStartToEnd(start, end);
+  Future<RouteDestination> getCoordsStartToEnd(LatLng start, LatLng end) async {
+    final trafficResponse =
+        await trafficService.getCoordsStartToEnd(start, end);
+    final distance = trafficResponse.routes[0].distance;
+    final duration = trafficResponse.routes[0].duration;
+    final geometry = trafficResponse.routes[0].geometry;
+    final points = decodePolyline(geometry, accuracyExponent: 6);
+
+    final latLngList = points
+        .map((coords) => LatLng(coords[0].toDouble(), coords[1].toDouble())).toList();
+
+    return RouteDestination(
+        points: latLngList, distance: distance, duration: duration);
+  }
 }
